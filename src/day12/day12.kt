6a7e9a419831df18489dca2ebe156144285cb747
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2024-12-01 20:46:17 +0100

    day 12 preparation

---
+++ src/day12/day12.kt
@@ -0,0 +1,32 @@
+package day12
+
+import printOutput
+import printTestOutput
+import readInput
+import readTestInput
+
+fun main() {
+    val day = 12
+    println("\n\n")
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readTestInput(day)
+    val part1Test = part1(testInput)
+    val part2Test = part2(testInput)
+
+    printTestOutput(testInput, part1Test, part2Test)
+
+    val input = readInput(day)
+    val part1 = part1(input)
+    val part2 = part2(input)
+
+    printOutput(part1, part2)
+}
